df811248409d91a5e8087bdf66b355050687de07
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-11-11 15:01:13 -0500

    Created layout and players
    Can currently move cards into player hands with number keys. No display
    change though.

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -16,6 +16,13 @@ public class Bartok : MonoBehaviour {
 	public List<CardBartok> drawPile;
 	public List<CardBartok> discardPile;
 
+	public BartokLayout layout;
+	public Transform layoutAnchor;
+
+	public float handFanDegrees = 10f;
+	public List<Player> players;
+	public CardBartok targetCard;
+
 
 	void Awake()
 	{
@@ -27,6 +34,84 @@ public class Bartok : MonoBehaviour {
 		deck = GetComponent<Deck> ();
 		deck.initDeck (deckXML.text);
 		Deck.shuffle (ref deck.cards);
+		layout = GetComponent<BartokLayout> ();
+		layout.readLayout (layoutXML.text);
+		drawPile = upgradeCardsList (deck.cards);
+		layoutGame ();
 	}
 
+	public void arrangeDrawPile()
+	{
+		CardBartok tCB;
+		for(int i=0; i<drawPile.Count;i++)
+		{
+			tCB = drawPile[i];
+			tCB.transform.parent =layoutAnchor;
+			tCB.transform.localPosition = layout.drawPile.pos;
+			tCB.faceUp = false;
+			tCB.setSortingLayerName(layout.drawPile.layerName);
+			tCB.setSortOrder(-i*4);
+			tCB.state = CBState.drawpile;
+		}
+	}
+
+	void layoutGame()
+	{
+		if (layoutAnchor == null)
+		{
+			GameObject tGO = new GameObject ("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+		arrangeDrawPile ();
+		Player pl;
+		players = new List<Player> ();
+		foreach (SlotDefB tSD in layout.slotDefs)
+		{
+			pl = new Player ();
+			pl.handSlotDef = tSD;
+			players.Add (pl);
+			pl.playerNum = players.Count;
+		}
+		players [0].type = PlayerType.human;
+	}
+
+	public CardBartok draw()
+	{
+		CardBartok cd = drawPile [0];
+		drawPile.RemoveAt (0);
+		return(cd);
+	}
+		
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			players[0].addCard(draw());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+		{
+			players [1].addCard (draw ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+		{
+			players [2].addCard (draw ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4))
+		{
+			players [3].addCard (draw ());
+		}
+	}
+
+
+	List<CardBartok> upgradeCardsList(List<Card> lCD)
+	{
+		List<CardBartok> lCB = new List<CardBartok> ();
+		foreach(Card tCD in lCD)
+		{
+			lCB.Add(tCD as CardBartok);
+		}
+		return lCB;
+	}
 }
---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum PlayerType
+{
+	human,
+	ai
+}
+
+[System.Serializable]
+public class Player {
+	public PlayerType type = PlayerType.ai;
+	public int playerNum;
+	public List<CardBartok> hand;
+	public SlotDefB handSlotDef;
+
+	public CardBartok addCard(CardBartok eCB)
+	{
+		if (hand == null) hand = new List<CardBartok> ();
+		hand.Add (eCB);
+		return eCB;
+	}
+	public CardBartok removeCard(CardBartok cb)
+	{
+		hand.Remove (cb);
+		return cb;
+	}
+}
